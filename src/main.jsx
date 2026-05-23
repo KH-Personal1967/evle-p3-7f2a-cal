3c8432a52c86ec4b96f9ed297b717228f7be828e
KH-Personal1967 <derek.newbauer@kimley-horn.com>
2026-05-23 10:36:20 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,4 @@ import "./index.css";
 
 import App from "./app.jsx";
 
-// Keep compatibility with your existing app.jsx pattern:
-window.React = React;
-window.ReactDOM = ReactDOM;
-
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
